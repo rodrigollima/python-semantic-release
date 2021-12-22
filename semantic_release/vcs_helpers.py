e62e59a4322a4c8c1b4cbe6bc9014f252eeff232
Rodrigo Lima <rodrigoxone@gmail.com>
2021-12-22 19:17:52 -0300

    Update vcs_helpers.py

--- semantic_release/vcs_helpers.py
+++ semantic_release/vcs_helpers.py
@@ -270,7 +270,7 @@ def push_new_version(
         )
         
         #repo.git.push(server)
-        repo.git.push("https://rodrigollima:fdsafasdfdsa@github.com/Samplemed/sample360-core.git", "python-semantic-release")
+        repo.git.push("https://rodrigollima:fdsafasdfdsa@github.com/Samplemed/sample360-core.git", "fix-python-release")
         repo.git.push("--tags", server)
     except GitCommandError as error:
         message = str(error)
